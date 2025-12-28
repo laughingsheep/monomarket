bfd888e6ee25e7f1d5552aa4fcb4edf5f69580c4
laughingsheep <jospashd@gmail.com>
2025-12-28 15:56:09 +0100

    Defer confetti.js

--- src/routes/api/explore/[topic]/[page]/+server.js
+++ src/routes/api/explore/[topic]/[page]/+server.js
@@ -2,7 +2,7 @@ import { json } from '@sveltejs/kit';
 
 export async function GET({params}) {
   let req;
-  const size = 300
+  const size = 250
   if(parseFloat(params.topic) === 0){
     req = await fetch("https://gamma-api.polymarket.com/markets?order=volume24hr&ascending=false&closed=false&limit=" + size + "&offset=" + size * (params.page -1))
   }else{
